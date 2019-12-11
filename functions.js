5be8298d3bb43b5d06fe023af336ce6e939d223f
Anjelica <anjelicacsilva@gmail.com>
2019-12-10 20:48:57 -0800

    Implement isHometown function

--- functions.js
+++ functions.js
@@ -3,10 +3,12 @@
 
 /** 1. isHometown */
 
-// Define your function here
+function isHometown(town) {
+  return town === 'San Francisco';
+}
 
 
-** 2. getFullName */
+/** 2. getFullName */
 
 // Define your function here
 
